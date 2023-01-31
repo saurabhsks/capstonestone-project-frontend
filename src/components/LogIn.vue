ce2447dab7cf137c2c5f16e69912f44d86d10608
Saurabh <saurabhsingh982001@gmail.com>
2023-01-31 13:56:24 +0530

    changes in style of login and signup page

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,16 +1,22 @@
 <template>
-    <div className="loginbg">
-  <h1 style="text-align: center">Login</h1>
-  <div class="login">
-    <input type="text" v-model="username" placeholder="Enter Username" />
-    <br />
-    <input type="password" v-model="password" placeholder="Enter Password" />
-    <br />
-    <div class="btn">
-      <button @click="login()" className="lbtn">Login</button>
+  <div className="loginbg">
+    <div class="style">
+      <h1 style="text-align: center">Login</h1>
+      <div class="login">
+        <input type="text" v-model="username" placeholder="Enter Username" />
+        <br />
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Enter Password"
+        />
+        <br />
+        <div class="btn">
+          <button @click="login()" className="lbtn">Login</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
   <router-view />
 </template>
 
@@ -52,6 +58,18 @@ export default {
 </script>
 
 <style scoped>
+.style {
+  background: rgba(6, 6, 6, 0.5);
+
+  font-size: 18px;
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.3);
+  color: #fff;
+  padding-bottom: 5%;
+  margin-left: 10%;
+  margin-right: 10%;
+}
 .login input {
   width: 30%;
   height: 40px;
@@ -61,10 +79,10 @@ export default {
   margin-left: auto;
   border: 1px solid skyblue;
 }
-.loginbg{
-    height:100vh;
-    background-size:cover;
-    background-image: url(https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
+.loginbg {
+  height: 100vh;
+  background-size: cover;
+  background-image: url(https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
 }
 .btn {
   width: 33.5%;
@@ -89,14 +107,14 @@ export default {
   font-weight: bold;
   font-size: large;
 }
-.lbtn:hover{
-    background-color: black;
-    border: 2px solid white;
-    color: #fff;
+.lbtn:hover {
+  background-color: black;
+  border: 2px solid white;
+  color: #fff;
 }
-.btn:hover{
-    background-color: black;
-    border: 2px solid white;
-    color: #fff;
+.btn:hover {
+  background-color: black;
+  border: 2px solid white;
+  color: #fff;
 }
 </style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="signupbg">
-        <div className="style">
-  <h1 style="text-align: center">SignUp</h1>
-  <div class="register">
-    <input type="text" v-model="username" placeholder="Enter UserName" /><br />
-    <input type="text" v-model="email" placeholder="Enter Email" /><br />
-    <input
-      type="password"
-      v-model="password"
-      placeholder="Enter Password"
-    /><br />
-    <div class="btn">
-      <button className="hover" v-on:click="signup()">Sign Up</button>
+  <div class="signupbg">
+    <div className="style">
+      <h1 style="text-align: center">SignUp</h1>
+      <div class="register">
+        <input
+          type="text"
+          v-model="username"
+          placeholder="Enter UserName"
+        /><br />
+        <input type="text" v-model="email" placeholder="Enter Email" /><br />
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Enter Password"
+        /><br />
+        <div class="btn">
+          <button className="hover" v-on:click="signup()">Sign Up</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
 </template>
 
 <script>
@@ -34,26 +38,23 @@ export default {
       console.log(this.username, this.email, this.password);
 
       if (this.username && this.email && this.password) {
-
-        if(this.password.length >=4 && this.password.length <=12){
-            const newuser = {
-          username: this.username,
-          email: this.email,
-          password: this.password,
-        };
-        const result = await axios.post(
-          "https://saurabhbookingapp.onrender.com/auth/register",
-         // "http://localhost:5000/auth/register",
-          newuser
-        );
-        console.log("Result is *****");
-        console.log(result);
-        this.$router.push({ path: "/login", replace: true });
+        if (this.password.length >= 4 && this.password.length <= 12) {
+          const newuser = {
+            username: this.username,
+            email: this.email,
+            password: this.password,
+          };
+          const result = await axios.post(
+            "https://saurabhbookingapp.onrender.com/auth/register",
+            // "http://localhost:5000/auth/register",
+            newuser
+          );
+          console.log("Result is *****");
+          console.log(result);
+          this.$router.push({ path: "/login", replace: true });
+        } else {
+          console.log("Please enter valid password");
         }
-        else{
-            console.log("Please enter valid password");
-        }
-        
       } else {
         console.log("Enter valid data");
       }
@@ -63,11 +64,11 @@ export default {
 </script>
 
 <style scoped>
-.signupbg{
-    height:100vh;
-    
-   background-size:cover;
-    background-image: url(https://media.istockphoto.com/id/512882668/photo/entering-hotel-room.jpg?s=612x612&w=0&k=20&c=dkStriVIj-19NVWg1AgHdBAtRGndus-sQJiZdo5N6ss=);
+.signupbg {
+  height: 100vh;
+
+  background-size: cover;
+  background-image: url(https://media.istockphoto.com/id/512882668/photo/entering-hotel-room.jpg?s=612x612&w=0&k=20&c=dkStriVIj-19NVWg1AgHdBAtRGndus-sQJiZdo5N6ss=);
 }
 .register input {
   width: 30%;
@@ -90,6 +91,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 40%;
 }
 
 .btn button {
@@ -102,22 +104,22 @@ export default {
   font-weight: bold;
   font-size: large;
 }
-.btn button:hover{
-    background-color: black;
-    border: 2px solid white;
-    color: #fff;
+.btn button:hover {
+  background-color: black;
+  border: 2px solid white;
+  color: #fff;
 }
-.style{
-    background: rgba(6, 6, 6, 0.5);
-  
-  font-size:18px;
+.style {
+  background: rgba(6, 6, 6, 0.5);
+
+  font-size: 18px;
   border-radius: 10px;
-  border:1px solid rgba(255,255,255,0.3);
-  box-shadow:2px 2px 15px
-  rgba(0,0,0,0.3);
-  color:#fff;
-  padding-top: 5%;
-  padding-bottom:5%
-  
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.3);
+  color: #fff;
+  padding-bottom: 5%;
+  margin-left: 10%;
+  margin-right: 10%;
 }
+  
 </style>
